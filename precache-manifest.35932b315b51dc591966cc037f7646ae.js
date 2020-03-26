136a445e17f9ff5eaf795758cf12b1df587e5006
Kyle Valdillez <kyle.valdillez@infor.com>
2020-03-25 22:18:57 -0400

    Updates

---
+++ precache-manifest.35932b315b51dc591966cc037f7646ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071f453ff96d5bb4fab13d1379ff46de",
+    "url": "/directory-app/index.html"
+  },
+  {
+    "revision": "9fffc95f920006bde0de",
+    "url": "/directory-app/static/css/main.a0eade40.chunk.css"
+  },
+  {
+    "revision": "fae3a7548c4c99e4fb65",
+    "url": "/directory-app/static/js/2.0f479795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/directory-app/static/js/2.0f479795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fffc95f920006bde0de",
+    "url": "/directory-app/static/js/main.93258d5c.chunk.js"
+  },
+  {
+    "revision": "fbbf08fc3f371274b3b9",
+    "url": "/directory-app/static/js/runtime-main.60f92d04.js"
+  }
+]);
